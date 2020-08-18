a179c5587fc0d420662acd8d9949dc0a8324587e
AndyLu <andyludeveloper@gmail.com>
2020-08-18 14:05:31 +0800

    Step6: Implement UserController which implement UserApi
    Keyword
    @RestController
    @Qualifier
    @Autowired

--- src/main/kotlin/com/example/spring_boot_kotlin_workshop/controller/impl/UserController.kt
+++ src/main/kotlin/com/example/spring_boot_kotlin_workshop/controller/impl/UserController.kt
@@ -1,4 +1,17 @@
 package com.example.spring_boot_kotlin_workshop.controller.impl
 
-class UserController {
+import com.example.spring_boot_kotlin_workshop.controller.UserApi
+import com.example.spring_boot_kotlin_workshop.data.dto.UserDto
+import com.example.spring_boot_kotlin_workshop.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController(
+        @Qualifier("DummyUserService") @Autowired val userService: UserService
+):UserApi {
+    override fun addUser(userDto: UserDto): UserDto = userService.addUser(userDto)
+
+    override fun findById(id: Long): UserDto = userService.findById(id)
 }
