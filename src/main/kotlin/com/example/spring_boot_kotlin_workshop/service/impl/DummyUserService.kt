72f51ebd1722f2bc3b60cfa7b33f7667c73d5476
AndyLu <andyludeveloper@gmail.com>
2020-08-18 13:53:12 +0800

    Step5: Implement a Service Component (DummyUserService) implement UserService
    Keyword
    @Service("DummyUserService")
    Service Component

--- src/main/kotlin/com/example/spring_boot_kotlin_workshop/service/impl/DummyUserService.kt
+++ src/main/kotlin/com/example/spring_boot_kotlin_workshop/service/impl/DummyUserService.kt
@@ -1,4 +1,13 @@
 package com.example.spring_boot_kotlin_workshop.service.impl
 
-class DummyUserService {
+import com.example.spring_boot_kotlin_workshop.data.dto.UserDto
+import com.example.spring_boot_kotlin_workshop.service.UserService
+import org.springframework.stereotype.Service
+
+@Service("DummyUserService")
+class DummyUserService : UserService{
+    override fun addUser(userDto: UserDto): UserDto = UserDto(-1, "Dummy, Service Add", 999)
+
+    override fun findById(id: Long): UserDto = UserDto(-1, "Dummy, Service Find", 999)
+
 }
