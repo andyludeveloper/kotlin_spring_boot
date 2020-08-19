570f27fdd3a699b34607f3a4cf8dc92323c4ff55
AndyLu <andyludeveloper@gmail.com>
2020-08-19 13:38:19 +0800

    Step 8: Define User entity

--- src/main/kotlin/com/example/spring_boot_kotlin_workshop/data/entity/User.kt
+++ src/main/kotlin/com/example/spring_boot_kotlin_workshop/data/entity/User.kt
@@ -1,4 +1,21 @@
 package com.example.spring_boot_kotlin_workshop.data.entity
 
-class User {
-}
+import javax.persistence.*
+
+@Entity
+@Table
+data class User (
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        val id: Long?,
+
+        @Column
+        val firstName:String,
+
+        @Column
+        val lastName:String,
+
+        @Column
+        val age: Int
+)
