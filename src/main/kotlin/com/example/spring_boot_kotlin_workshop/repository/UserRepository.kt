3fdb8216bf8d3c5ef6569fb1552691e0db776a8c
AndyLu <andyludeveloper@gmail.com>
2020-08-19 13:57:50 +0800

    Step 10: Define UserRepository

--- src/main/kotlin/com/example/spring_boot_kotlin_workshop/repository/UserRepository.kt
+++ src/main/kotlin/com/example/spring_boot_kotlin_workshop/repository/UserRepository.kt
@@ -1,4 +1,11 @@
 package com.example.spring_boot_kotlin_workshop.repository
 
-class UserRepository {
+import com.example.spring_boot_kotlin_workshop.data.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository: JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
 }
